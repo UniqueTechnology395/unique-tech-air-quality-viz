63eb280e88c5013fa22b37bad4923f64e27ec54e
UniqueTechnology395 <UniqueTech2025@outlook.com.gr>
2026-04-26 11:44:55 +0300

    Update sensor_reader.ino

--- src/sensor_reader.ino
+++ src/sensor_reader.ino
@@ -1,5 +1,18 @@
-// Unique Tech - Sensor Data Sender
-// Project: ACSTAC 2026 Innovation
+/* * ----------------------------------------------------------------------------
+ * Project: Unique Tech Air Quality Visualization
+ * Author: Unique Tech Team
+ * License: MIT License
+ * * Copyright (c) 2026 Unique Tech
+ * * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * ----------------------------------------------------------------------------
+ */
 
 void setup() {
   Serial.begin(9600); // Ξεκινάμε τη σειριακή επικοινωνία
